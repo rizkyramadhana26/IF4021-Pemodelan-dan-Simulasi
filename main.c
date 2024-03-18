57fb832aa6675815128d1716f6733ba2f9a46f54
Rizky <rizkyramadhana26@gmail.com>
2024-03-18 13:49:39 +0700

    define constant

---
+++ main.c
@@ -0,0 +1,37 @@
+#include "simlib.h"
+
+#define PERSON_ARRIVED_1         1  
+#define PERSON_ARRIVED_2         2  
+#define PERSON_ARRIVED_3         3  
+#define BUS_ARRIVED_1            4  
+#define BUS_ARRIVED_3            6  
+#define BUS_ARRIVED_2            5  
+#define BUS_DEPARTED_1           7  
+#define BUS_DEPARTED_2           8  
+#define BUS_DEPARTED_3           9  
+#define UNLOAD_1                10 
+#define UNLOAD_2                11 
+#define UNLOAD_3                12 
+#define LOAD_1                  13 
+#define LOAD_2                  14 
+#define LOAD_3                  15 
+#define QUEUE_1                  1  
+#define QUEUE_2                  2  
+#define QUEUE_3                  3  
+#define SERVER                   4  
+#define SAMPST_DELAYS_1          1  
+#define SAMPST_DELAYS_2          2  
+#define SAMPST_DELAYS_3          3  
+#define SAMPST_SERVER_1          4  
+#define SAMPST_SERVER_2          5  
+#define SAMPST_SERVER_3          6  
+#define SAMPST_LOOP_1            7  
+#define SAMPST_LOOP_2            8  
+#define SAMPST_LOOP_3            9  
+#define SAMPST_PERSON           10 
+#define INTERVAL_ARRIVAL_1       1  
+#define INTERVAL_ARRIVAL_2       2  
+#define INTERVAL_ARRIVAL_3       3  
+#define INTERVAL_UNLOADING       4  
+#define INTERVAL_LOADING         5  
+#define INTERVAL_DESTINATION     6  
